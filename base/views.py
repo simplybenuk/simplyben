01f0fb1eec927eb4c760b88bc2441d31fdd4d521
Benjamin Heap <benjaminpheap@gmail.com>
2018-12-24 14:28:46 +0000

    Delete views.py

--- base/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import HttpResponse
-from django.shortcuts import render, get_object_or_404
-
-# Create your views here.
-
-def home(request):
-    return render(request, "index.html", {})
