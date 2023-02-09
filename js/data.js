ce1dae54d3cc8ef71d6080956c08e195664e5204
Ben Whitfield-Heap <benjaminpheap@gmail.com>
2023-02-09 12:55:32 +0000

    Create data.js
    Created new data.js file.
    To be pulled into the latest updates section

---
+++ js/data.js
@@ -0,0 +1,21 @@
+export const updateData = [
+  { 
+    date : 'Feb 23',
+    update : '<p></p>'
+   },
+  { 
+    date : 'Jan 23',
+    update : '<p></p>'
+   }, 
+  { 
+    date : 'Dec 22',
+    update : '<p></p>'
+   },
+  { 
+    date : 'Nov 22',
+    update : '<p>Come on Elon, try and take this down!
+                (also follow me on Twitter <a target="_blank " href="https://twitter.com/simplybenuk">@simplybenuk</a>
+                or Mastodon! <a target="_blank" href="https://mastodon.social/@B13">mastodon.social/@B13</a>)</p>'
+   },
+  ]
+  
