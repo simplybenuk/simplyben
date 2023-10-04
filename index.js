81e63cf19766d15055d673038b6e1a38f763079e
Ben Whitfield-Heap <benjaminpheap@gmail.com>
2023-10-04 13:53:33 +0100

    Test

---
+++ index.js
@@ -0,0 +1,29 @@
+fetch('status.md')
+  .then(response => response.text())
+  .then(text => {
+    const statuses = text.split('\n\n');
+    const displayStatuses = statuses.slice(0, 5);
+
+    let statusHTML = "";
+    for (let status of displayStatuses) {
+      const [date, ...content] = status.split(": ");
+      statusHTML += `
+        <div class="status">
+          <div class="status-item status-avatar">
+            <img src="img/avatar.jpg" alt="profile picture">
+          </div>
+          <div class="status-item status-name">
+            <span><strong>Ben Whitfield-Heap</strong></span> | <span>${date}</span>
+          </div>
+          <div class="status-item status-content">
+            <p>${content.join(": ")}</p>
+          </div>
+        </div>
+      `;
+    }
+    
+    document.getElementById('status-list').innerHTML = statusHTML;
+  })
+  .catch(error => {
+    console.error('Error fetching status:', error);
+  });
---
+++ status.md
@@ -0,0 +1,15 @@
+Nov 22: Come on Elon, try and take this down!
+(also follow me on Twitter [simplybenuk](https://twitter.com/simplybenuk)
+or Mastodon! [mastodon.social/@B13](https://mastodon.social/@B13))
+
+
+Sep 15: Excited about the new project at work. Can't wait to share more details soon!
+
+
+Aug 10: Just completed a 10km run. Feeling great!
+
+
+Jul 21: Had a fantastic meeting with the team today. Lots of ideas for upcoming projects.
+
+
+Jun 30: Cooked a delicious vegan meal tonight. Would highly recommend!
